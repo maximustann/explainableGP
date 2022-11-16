ac65333f81705f03c506c2c617560ba6862df60e
Maximus Tann <maximustann@MaximusdeMacBook-Pro.local>
2022-11-16 23:36:11 +1300

    update pass capacity bar

--- static/js/drawMap.js
+++ static/js/drawMap.js
@@ -29,11 +29,13 @@ class RouteMap {
       .scaleLinear()
       .domain([0, this.maxY])
       .range([this.margin, this.height - this.margin * 2]);
+
+    this.barScaler = d3.scaleLinear().domain([0, 1]).range([0, 40]);
   }
 
   // The following functions return static values
-  static get DEPOT(){
-    return 0
+  static get DEPOT() {
+    return 0;
   }
 
   static get TASK() {
@@ -77,6 +79,7 @@ class RouteMap {
     const maxY = this.maxY;
     var widthScaler = this.widthScaler;
     var heightScaler = this.heightScaler;
+    var barScaler = this.barScaler;
 
     this.svg = d3
       .select("#Map")
@@ -114,6 +117,7 @@ class RouteMap {
         ) {
           link["property"] = bind;
           link["FULL"] = bind["FULL"];
+          link["Capacity"] = 1;
         }
       }
     }
@@ -190,20 +194,7 @@ class RouteMap {
       initialPoint.target
     );
 
-    this.truck = this.g
-      .append("image")
-      .attr("class", "image")
-      .attr("xlink:href", "../static/image/truck.png")
-      .attr(
-        "transform",
-        "translate(" +
-          (this.widthScaler(initialPosition.sourceX) - RouteMap.OFFSET) +
-          ", " +
-          (this.heightScaler(initialPosition.sourceY) - RouteMap.OFFSET) +
-          ")"
-      )
-      .attr("width", 40)
-      .attr("height", 40);
+
 
     var capacityData = [100, 0];
     this.capacity = this.svg
@@ -248,8 +239,8 @@ class RouteMap {
       .append("text")
       .style("font-size", "10px")
       .attr("dy", ".25em")
-      .attr("y", 10)
-      .attr("x", 10)
+      .attr("y", -10)
+      .attr("x", -15)
       .text(function (d) {
         if (d.depot == true) {
           return "depot";
@@ -282,7 +273,7 @@ class RouteMap {
         return d;
       })
       .attr("transform", "translate(15, 9)")
-      .style('font-size', "10px");
+      .style("font-size", "10px");
 
     this.legend
       .append("line")
@@ -306,34 +297,164 @@ class RouteMap {
         }
       });
 
-      let capacityLegend = this.svg.append('g')
-          .attr('class', 'capacityLegend')
-          .attr("transform", "translate(" + widthScaler(legendData.length * 1.2) + "," + (heightScaler(maxY) + margin) + ")")
-          .selectAll('arc')
-          .data(pie(capacityData))
-          .enter()
-          .append("g")
-          .append('path')
-          .attr("fill", (data, i) => {
-            let value = data.data;
-            return d3.schemeSet3[i];
-          })
-          .attr("d", arc);
+    let capacityLegend = this.svg
+      .append("g")
+      .attr("class", "capacityLegend")
+      .attr(
+        "transform",
+        "translate(" +
+          widthScaler(legendData.length * 1.2) +
+          "," +
+          (heightScaler(maxY) + margin) +
+          ")"
+      )
+      .selectAll("arc")
+      .data(pie(capacityData))
+      .enter()
+      .append("g")
+      .append("path")
+      .attr("fill", (data, i) => {
+        let value = data.data;
+        return d3.schemeSet3[i];
+      })
+      .attr("d", arc);
 
-        this.svg.select(".capacityLegend")
-              .append("text")
-          .style("font-size", "5px")
-          .attr("dy", ".25em")
-          .attr("dx", "-1.2em")
-          .text('100%')
-        
-          this.svg.select(".capacityLegend")
-              .append("text")
-              .text("Remaining Capacity")
-              .attr("transform", "translate(15, 9)")
-              .style('font-size', "10px");
+    this.svg
+      .select(".capacityLegend")
+      .append("text")
+      .style("font-size", "5px")
+      .attr("dy", ".25em")
+      .attr("dx", "-1.2em")
+      .text("100%");
 
+    this.svg
+      .select(".capacityLegend")
+      .append("text")
+      .text("Remaining Capacity")
+      .attr("transform", "translate(15, 9)")
+      .style("font-size", "10px");
 
+    var bar = this.svg
+      .append("g")
+      .attr("class", "gb")
+      .selectAll("rect")
+      .data(this.links)
+      .enter()
+      .append("rect")
+      .attr("width", function (d) {
+        if (d.task != RouteMap.TASK) {
+          return 0;
+        }
+        // if the path is horizontal
+        if (d.source["x"] != d.target["x"]) {
+          return barScaler(1);
+        } else {
+          return barScaler(0.2);
+        }
+      })
+      .attr("height", function (d) {
+        if (d.task != RouteMap.TASK) {
+          return 0;
+        }
+        // if the path is horizontal
+        if (d.source["x"] != d.target["x"]) {
+          return barScaler(0.2);
+        } else {
+          return barScaler(1);
+        }
+      })
+      .attr("x", function (d) {
+        if (d.task != RouteMap.TASK) {
+          return 0;
+        }
+        // if the path is horizontal
+        if (d.source["x"] != d.target["x"]) {
+          return widthScaler(d.source["x"] + 0.3);
+        } else {
+          return widthScaler(d.target["x"] + 0.1);
+        }
+      })
+      .attr("y", function (d, i) {
+        if (d.task != RouteMap.TASK) {
+          return 0;
+        }
+        // if the path is horizontal
+        if (d.source["x"] != d.target["x"]) {
+          return heightScaler(d.target["y"] + 0.1);
+        } else {
+          return heightScaler(d.target["y"] + 0.2);
+        }
+      })
+      .attr("fill", "lightgrey");
+
+    // console.log(this.links)
+    this.progress = this.svg
+      .append("g")
+      .attr("class", "progress")
+      .selectAll("rect")
+      .data(this.links)
+      .enter()
+      .append("rect")
+      .attr("width", function (d, i) {
+        if (d.task != RouteMap.TASK) {
+          return 0;
+        }
+        // if the path is horizontal
+        if (d.source["x"] != d.target["x"]) {
+          return barScaler(1);
+        } else {
+          return barScaler(0.2);
+        }
+      })
+      .attr("height", function (d) {
+        if (d.task != RouteMap.TASK) {
+          return 0;
+        }
+        // if the path is horizontal
+        if (d.source["x"] != d.target["x"]) {
+          return barScaler(0.2);
+        } else {
+          return barScaler(1);
+        }
+      })
+      .attr("x", function (d) {
+        if (d.task != RouteMap.TASK) {
+          return 0;
+        }
+        // if the path is horizontal
+        if (d.source["x"] != d.target["x"]) {
+          return widthScaler(d.source["x"] + 0.3);
+        } else {
+          return widthScaler(d.target["x"] + 0.1);
+        }
+      })
+      .attr("y", function (d, i) {
+        if (d.task != RouteMap.TASK) {
+          return 0;
+        }
+        // if the path is horizontal
+        if (d.source["x"] != d.target["x"]) {
+          return heightScaler(d.target["y"] + 0.1);
+        } else {
+          return heightScaler(d.target["y"] + 0.2);
+        }
+      })
+      .attr("fill", "red");
+
+      this.truck = this.g
+      .append("image")
+      .attr("class", "image")
+      .attr("xlink:href", "../static/image/truck.png")
+      .attr(
+        "transform",
+        "translate(" +
+          (this.widthScaler(initialPosition.sourceX) - RouteMap.OFFSET) +
+          ", " +
+          (this.heightScaler(initialPosition.sourceY) - RouteMap.OFFSET) +
+          ")"
+      )
+      .attr("width", 40)
+      .attr("height", 40);
   }
 
   // Move the truck to previous point
@@ -394,6 +515,9 @@ class RouteMap {
   }
 
   async driveForward() {
+    var barScaler = this.barScaler;
+    var widthScaler = this.widthScaler;
+    var heightScaler = this.heightScaler;
     // the reaction to the "next" button, run on the path and change the color of the edges
 
     // if the path reaches the length of the data, reset to the starting point
@@ -410,28 +534,33 @@ class RouteMap {
       );
       // console.log(targetPosition.remainingCapacity)
 
-      // console.log(this.capacityRemaining, targetPosition.FULL)
-      if (targetPosition.remainingCapacity == undefined && targetPoint.target != RouteMap.DEPOT) {
+      // if the truck drive back to the depot, the capacityRemaining restore to 100
+      if (
+        targetPosition.remainingCapacity == undefined &&
+        targetPoint.target != RouteMap.DEPOT
+      ) {
         var capacityData = [
           this.capacityRemaining,
           100 - this.capacityRemaining,
         ];
-      } else if(targetPosition.remainingCapacity == undefined && targetPoint.target == RouteMap.DEPOT){
-          this.capacityRemaining = 100
-          var capacityData = [
-            this.capacityRemaining,
-            100 - this.capacityRemaining
-          ]
+      } else if (
+        targetPosition.remainingCapacity == undefined &&
+        targetPoint.target == RouteMap.DEPOT
+      ) {
+        this.capacityRemaining = 100;
+        var capacityData = [
+          this.capacityRemaining,
+          100 - this.capacityRemaining,
+        ];
       } else {
-          this.capacityRemaining = targetPosition.remainingCapacity;
-            var capacityData = [
-              this.capacityRemaining,
-              100 - this.capacityRemaining,
-            ];
-        
+        this.capacityRemaining = targetPosition.remainingCapacity;
+        var capacityData = [
+          this.capacityRemaining,
+          100 - this.capacityRemaining,
+        ];
       }
 
-      // console.log(capacityData)
+      // console.log(targetPoint)
 
       // wait until the animation finishes
       await d3
@@ -465,16 +594,9 @@ class RouteMap {
           return d3.schemeSet3[i];
         })
         .attr("d", arc);
-      // .append("g")
 
-      // arcs.append("path")
-      //     .attr("fill", (data, i) => {
-      //       let value = data.data;
-      //       return d3.schemeSet3[i];
-      //     })
-      //     .attr("d", arc)
-
-      this.capacity.select("text")
+      this.capacity
+        .select("text")
         .data(capacityData)
         .style("font-size", "5px")
         .attr("dy", ".25em")
@@ -510,6 +632,84 @@ class RouteMap {
             return 0;
           }
         });
+
+      // update the link capacity
+      for (let link of this.links) {
+        // console.log(link)
+        if (
+          (link.task == RouteMap.TASK &&
+            link.source.id == targetPoint.source &&
+            link.target.id == targetPoint.target) ||
+          (link.task == RouteMap.TASK &&
+            link.source.id == targetPoint.target &&
+            link.target.id == targetPoint.source)
+        ) {
+          for (let bind of this.bindData) {
+            if (
+              (bind.source == targetPoint.source &&
+                bind.target == targetPoint.target) ||
+              (bind.target == targetPoint.source &&
+                bind.source == targetPoint.target)
+            ) {
+              link["Capacity"] = bind.PASSCAP;
+            }
+          }
+        }
+      }
+
+      this.progress.remove();
+      this.progress = this.svg
+        .append("g")
+        .attr("class", "progress")
+        .selectAll("rect")
+        .data(this.links)
+        .enter()
+        .append("rect")
+        .attr("width", function (d, i) {
+          if (d.task != RouteMap.TASK) {
+            return 0;
+          }
+          // if the path is horizontal
+          if (d.source["x"] != d.target["x"]) {
+            return barScaler(d.Capacity);
+          } else {
+            return barScaler(0.2);
+          }
+        })
+        .attr("height", function (d) {
+          if (d.task != RouteMap.TASK) {
+            return 0;
+          }
+          // if the path is horizontal
+          if (d.source["x"] != d.target["x"]) {
+            return barScaler(0.2);
+          } else {
+            return barScaler(d.Capacity);
+          }
+        })
+        .attr("x", function (d) {
+          if (d.task != RouteMap.TASK) {
+            return 0;
+          }
+          // if the path is horizontal
+          if (d.source["x"] != d.target["x"]) {
+            return widthScaler(d.source["x"] + 0.3);
+          } else {
+            return widthScaler(d.target["x"] + 0.1);
+          }
+        })
+        .attr("y", function (d, i) {
+          if (d.task != RouteMap.TASK) {
+            return 0;
+          }
+          // if the path is horizontal
+          if (d.source["x"] != d.target["x"]) {
+            return heightScaler(d.target["y"] + 0.1);
+          } else {
+            return heightScaler(d.target["y"] + 0.2);
+          }
+        })
+        .attr("fill", "red");
     }
     this.path += 1;
   }
@@ -525,6 +725,7 @@ class RouteMap {
           targetY: obj.target["y"],
           task: obj.task,
           remainingCapacity: obj.FULL,
+          // passCapacity: obj.PASSCAP
         };
       }
 
@@ -536,6 +737,7 @@ class RouteMap {
           targetY: obj.source["y"],
           task: obj.task,
           remainingCapacity: obj.FULL,
+          // passCapacity: obj.PASSCAP
         };
       }
     }
